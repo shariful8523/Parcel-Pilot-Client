57280b529d4dcf9a8484df7969f1286900d62aa9
Md. Shariful Islam <mdsharifulislam8523@gmail.com>
2025-10-19 19:40:57 +0600

    update Banner image

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import { Carousel } from 'react-responsive-carousel';
 
 import Banner1 from '../../assets/banner/banner1.png'
 import Banner2 from '../../assets/banner/banner2.png'
-import Banner3 from '../../assets/banner/banner3.png'
+import Banner5 from '../../assets/banner/banner5.png'
 
 const Banner = () => {
     return (
@@ -15,7 +15,7 @@ const Banner = () => {
                 <img src={Banner2} alt="Banner 2" />
             </div>
             <div>
-                <img src={Banner3} alt="Banner 3" />
+                <img src={Banner5} alt="Banner 3" />
             </div>
         </Carousel>
     );
