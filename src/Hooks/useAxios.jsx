2c81849c90a9d6d8d5f8a21eeaaaa06a8e40827b
Md. Shariful Islam <mdsharifulislam8523@gmail.com>
2025-10-19 20:09:04 +0600

    serverside connect

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: `http://localhost:5000`
+    baseURL: `https://parcel-pilot-server-mu.vercel.app`
 })
 
 const useAxios = () => {
--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -7,7 +7,7 @@ const useAxiosSecure = () => {
   const navigate = useNavigate();
 
   const axiosSecure = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://parcel-pilot-server-mu.vercel.app/",
   });
 
   // Request interceptor
